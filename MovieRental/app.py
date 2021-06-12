bebc6322f9d162db8a6350a69cbd28f8203fcff9
Shashank <s_shashank@outlook.com>
2021-06-12 22:56:18 +0530

    numpy arrays

--- MovieRental/app.py
+++ MovieRental/app.py
@@ -5,7 +5,7 @@ user = User("Rahul")
 
 # user.add_movie("The Matrix", "Sci-Fi")
 # user.add_movie("Harry Potter", "Fantasy")
-#
+
 # print(user.save_as_json())
 
 load_data = User.load_from_json(user.name)
