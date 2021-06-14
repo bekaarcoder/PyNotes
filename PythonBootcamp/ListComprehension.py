1c5b8b2136d9a43644447f9a4ed64acaeec66aae
Shashank <s_shashank@outlook.com>
2021-06-14 22:38:22 +0530

    indexing, filtering, update row and column

--- PythonBootcamp/ListComprehension.py
+++ PythonBootcamp/ListComprehension.py
@@ -1,8 +1,19 @@
-cities = ["London", "NY", "Paris"]
+# cities = ["London", "NY", "Paris"]
 
 
-def visit(city):
-    print(f"Welcome to {city}")
+# def visit(city):
+#     print(f"Welcome to {city}")
 
 
-[visit(x) for x in cities]
+# [visit(x) for x in cities]
+
+output_using_for = []
+
+for x in range(1, 11):
+    output_using_for.append(x ** 2)
+
+print(f"Squares using for loop: {output_using_for}")
+
+output_using_list_comp = [x ** 2 for x in range(1, 11)]
+
+print(f"Squares using list comprehension: {output_using_list_comp}")
